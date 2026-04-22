8379f818be92eb21db3969fc3ea7fca3223d63eb
rommel dalisay <rommelmdalisay@gmail.com>
2026-04-22 15:39:07 +0800

    dynamic propertry

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Customer;
+use App\Models\User;
+use App\Models\Order;
+use App\Models\Item;
+use Storage;
 
 class CustomerController extends Controller
 {
@@ -11,7 +16,12 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        //
+        // $customers = Customer::all();
+        // // dd($customers);
+        // return response()->json($customers);
+        // $customer = User::find(1)->customer;
+        // $user = Customer::find(36)->user->name;
+
     }
 
     /**
@@ -27,7 +37,27 @@ class CustomerController extends Controller
      */
     public function show(string $id)
     {
-        //
+        // $orders = Customer::find($id)->orders;
+        // // dd($orders);
+        // foreach ($orders as $order) {
+        //     dump($order->orderinfo_id, $order->placed);
+        // }
+        // $customer = Order::find($id)->customer;
+        // dd($customer);
+
+        // $items = Order::find($id)->items;
+
+        // foreach ($items as $item) {
+        //     dump($item->description, $item->sell_price);
+        // }
+        // dd($items);
+
+        $orders = Item::find($id)->orders;
+
+        // foreach ($items as $item) {
+        //     dump($item->description, $item->sell_price);
+        // }
+        dd($orders);
     }
 
     /**
