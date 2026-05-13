52fd24bdce01e218d854893c281bffaa6ddf6c5f
rommel dalisay <rommelmdalisay@gmail.com>
2026-05-13 15:37:17 +0800

    chart api

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,46 +3,58 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class DashboardController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function addressChart()
     {
-        //
+        $customer = DB::table('customer')
+            ->groupBy('addressline')
+            ->orderBy('total', 'DESC')
+            ->pluck(DB::raw('count(addressline) as total'), 'addressline')
+            ->all();
+        // dd($customer);
+        $labels = (array_keys($customer));
+        $data = array_values($customer);
+        // dd($customer, $data, $labels);
+        return response()->json(array('values' => $data, 'labels' => $labels));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function salesChart()
     {
-        //
+
+        $sales = DB::table('item as i')
+            ->join('orderline as ol', 'i.item_id', '=', 'ol.item_id')
+            ->join('orderinfo as oi', 'ol.orderinfo_id', '=', 'oi.orderinfo_id')
+            ->select(DB::raw('monthname(oi.date_placed) as month, sum(ol.quantity * i.sell_price) as total'))
+            ->groupBy('oi.date_placed')
+            ->pluck('total', 'month')
+            ->all();
+
+        // dd($sales);
+        $labels = (array_keys($sales));
+
+        $data = array_values($sales);
+        // dd($sales, $data, $labels);
+        return response()->json(array('values' => $data, 'labels' => $labels));
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function itemsChart()
     {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
+        $items = DB::table('item as i')
+            ->join('orderline as ol', 'i.item_id', '=', 'ol.item_id')
+            ->select(DB::raw('i.description as items, sum(ol.quantity) as total'))
+            ->groupBy('i.description')
+            ->pluck('total', 'items')
+            ->all();
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        // dd($items);
+        $labels = (array_keys($items));
+
+        $data = array_values($items);
+        // dd($items, $data, $labels);
+        return response()->json(array('values' => $data, 'labels' => $labels));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,6 +12,10 @@ Route::get('/user', function (Request $request) {
 
 
 Route::prefix('v1')->group(function () {
+    Route::get('dashboard/address-chart', [DashboardController::class, 'addressChart']);
+    Route::get('dashboard/sales-chart', [DashboardController::class, 'salesChart']);
+    Route::get('dashboard/items-chart', [DashboardController::class, 'itemsChart']);
+
     Route::apiResource('customers', CustomerController::class);
     Route::apiResource('items', ItemController::class);
     Route::post('/items/checkout', [ItemController::class, 'postCheckout'])->name('postCheckout');
